e43898e36802034be9d706867fe6b2c4b101685c
njofredev <nico.jofreandrade@gmail.com>
2024-03-05 13:11:24 -0300

    Add code into actividad2

--- actividad2/sistema_bancario.py
+++ actividad2/sistema_bancario.py
@@ -7,3 +7,26 @@ Nicolás Jofré Andrade
 que representen cuentas bancarias y clientes con atributos como: saldo, nombre titular 
 y métodos para realizar transacciones.
 """
+class Cuentabancaria:
+
+    def __init__(self, nro_cuenta, nombre, saldo):
+        self.nro_cuenta = nro_cuenta
+        self.nombre = nombre 
+        self.saldo = saldo
+
+    def mostrarSaldo(self):
+        print(f"El usuario: {self.nombre}, su cuenta:{self.nro_cuenta}, tiene de saldo actual {self.saldo}")
+
+class Cliente:
+
+    def __init__(self, rut, nombre, apellido):
+        self.rut = rut
+        self.nombre = nombre 
+        self.apellido = apellido
+
+    def depositar(self, saldo):
+        self.saldo += saldo
+        return f"El saldo de {self.nombre}, Cuenta: {self.nro_cuenta}, tiene de saldo: {self.saldo}"
+
+    def retirar(self, saldo):
+        pass
