806e02c7395f9b3dbb642cb0fc364cb3a2e55a4c
njofredev <nico.jofreandrade@gmail.com>
2024-03-08 12:35:33 -0300

    Calculadora.py start

--- actividad5/calculadora.py
+++ actividad5/calculadora.py
@@ -6,4 +6,28 @@ Nicolás Jofré Andrade
 - Implementar una calculadora simple en python que maneje errores y excepciones. Debe
 realizar operaciones básicas como: suma, resta, multiplicación y división. Debe manejar
 errores y excepciones como la división por 0 y la entrada incorrecta del usuario.
+- metas: usar assert() e isinstance() para la comprobación de erorres
 """
+class Calculadora:
+    
+    def __init__(self, operacion):
+        self.operacion = operacion
+        
+class Operacion:
+    pass
+    
+    def sumar(self):
+        
+        assert isinstance(int) # Assert para verificar el dato que quiero en la variable a
+        assert isinstance(int)
+
+    def restar(self, a, b):
+        pass
+
+    def multiplicar(self, a, b):
+        pass
+
+    def dividir(self, a, b):
+        pass
+
+        
