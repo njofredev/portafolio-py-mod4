ebd0b65ca26437b10fed5f2b528d95806dc3e2dc
njofredev <nico.jofreandrade@gmail.com>
2024-03-11 13:05:35 -0300

    Add calculadora.py solution

--- actividad5/calculadora.py
+++ actividad5/calculadora.py
@@ -6,36 +6,83 @@ Nicolás Jofré Andrade
 - Implementar una calculadora simple en python que maneje errores y excepciones. Debe
 realizar operaciones básicas como: suma, resta, multiplicación y división. Debe manejar
 errores y excepciones como la división por 0 y la entrada incorrecta del usuario.
-- metas: usar assert() e isinstance() para la comprobación de erorres
-"""
-"""class Calculadora:
-    
-    def __init__(self, operacion):
-        self.operacion = operacion""" # Suma, resta, multiplicacion, division
-        
+"""     
 class Operacion:
     
-    def __init__(self, num_1, num_2, operacion, resultado):
-        self.num_1 = num_1
-        self.num_2 = num_2
-        self.operacion = operacion
-        self.resultado = resultado
+    def __init__(self):
+        pass
     
     def sumar(self):
-        self.num_1 = int(input("Ingrese el primero número: \n"))
-        self.num_2 = int(input("Ingrese el segundo número: \n"))
+        while True:
+            try:
+                self.num_1 = int(input("Ingrese el primer número: \n"))
+                self.num_2 = int(input("Ingrese el segundo número: \n"))
 
-        print(f"La suma de {self.num_1} y {self.num_2} es: {self.num_1 + self.num_2}")
+                print(f"La suma de {self.num_1} y {self.num_2} es: {self.num_1 + self.num_2}")
+                break
+            except ValueError:
+                print("ERROR: Ingrese un valor numérico válido.")
+                break
 
-    def restar(self, a, b):
-        pass
+    def restar(self):
+        while True:
+            try:
+                self.num_1 = int(input("Ingrese el primer número: \n"))
+                self.num_2 = int(input("Ingrese el segundo número: \n"))
 
-    def multiplicar(self, a, b):
-        pass
+                print(f"La resta de {self.num_1} y {self.num_2} es: {self.num_1 - self.num_2}")
+                break
+            except ValueError:
+                print("ERROR: Ingrese un valor numérico válido.")
+                break
 
-    def dividir(self, a, b):
-        pass
+    def multiplicar(self):
+        while True:
+            try:
+                self.num_1 = int(input("Ingrese el primer número: \n"))
+                self.num_2 = int(input("Ingrese el segundo número: \n"))
 
-calculadora = Operacion(1, 2, "suma", 3)
+                print(f"La multiplicación de {self.num_1} y {self.num_2} es: {self.num_1 * self.num_2}")
+                break
+            except ValueError:
+                print("ERROR: Ingrese un valor numérico válido.")
+                break
 
-calculadora.sumar()
+    def dividir(self):
+        while True:
+            try:
+                self.num_1 = int(input("Ingrese el primero número: \n"))
+                self.num_2 = int(input("Ingrese el segundo número: \n"))
+
+                print(f"La división entre {self.num_1} y {self.num_2} es: {self.num_1 / self.num_2}")
+                break
+            except ZeroDivisionError:
+                print("ERROR: Está intentando dividir por 0.")
+                break
+        
+
+# Instancia de la clase
+calculadora = Operacion()            
+
+while True:
+    print("\n---Calculadora básica---")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir del programa")
+    opcion_menu = int(input("Ingrese una opción del menú: \n"))
+
+    if opcion_menu == 1:
+        calculadora.sumar()
+    elif opcion_menu == 2:
+        calculadora.restar()
+    elif opcion_menu == 3:
+        calculadora.multiplicar()
+    elif opcion_menu == 4:
+        calculadora.dividir()
+    elif opcion_menu == 5:
+        print("Gracias por participar en la calculadora. Saliendo..")
+        break
+    else:
+        print("ERROR: Ingrese un valor del menú.")
