a2b5300c21e1f92ca7f2f207436650f93194e703
njofredev <nico.jofreandrade@gmail.com>
2024-03-15 12:00:36 -0300

    Add fix vehiculo.py

--- proyecto-final-modulo-4/vehiculo.py
+++ proyecto-final-modulo-4/vehiculo.py
@@ -5,7 +5,7 @@ Nicolás Jofré Andrade
 - Crea un archivo llamado vehiculo.py
 - Definir clase llamada Vehiculo
     - Atributos: marca, modelo, color y velocidad
-    - Métodos: acelerar() - Aumenta velocidad en 10 unidades
+    - Métodos: acelerar() + Aumenta velocidad en 10 unidades
                frenar() - Disminuye velocidad en 5 unidades
 
 - Imprimir estado del vehículo después de cada método
