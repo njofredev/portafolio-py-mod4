e6941623f6e1760e8bbf1adb6212f7e07a2f2595
njofredev <nico.jofreandrade@gmail.com>
2024-03-15 11:59:18 -0300

    Add vehiculo.py solution

--- proyecto-final-modulo-4/vehiculo.py
+++ proyecto-final-modulo-4/vehiculo.py
@@ -13,30 +13,28 @@ Nicolás Jofré Andrade
 # Clase de vehículo
 class Vehiculo:
 
-    def __init__(self, marca, modelo, color, velocidad, velocidad_actual):
+    def __init__(self, marca, modelo, color, velocidad):
         self.marca = marca
         self.modelo = modelo
         self.color = color
         self.velocidad = velocidad
-        self.velocidad_actual = velocidad_actual
     
     def datos_actuales(self):
-        print(f"Los datos del auto son: Marca:{self.marca} | Modelo:{self.modelo} | Color:{self.color}| Velocidad actual: {self.velocidad_actual}km/h")
+        print(f"Los datos del auto son: Marca:{self.marca} | Modelo:{self.modelo} | Color:{self.color}| Velocidad actual: {self.velocidad}km/h")
 
     def acelerar(self):
         self.velocidad += 10
         print(f"El auto marca: {self.marca}, modelo: {self.modelo}, color: {self.color} va a: {self.velocidad}km/h")
         
     def frenar(self):
-        self.velocidad -= 5
-
         if self.velocidad <= 0:
             print("El vehiculo se encuentra detenido")
         else:
+            self.velocidad -= 5
             print(f"El auto marca: {self.marca}, modelo: {self.modelo}, color: {self.color} va a: {self.velocidad}km/h")
 
 # Instancia de la clase
-vehiculo = Vehiculo("BMW", "X4", "Negro", 0, 0)
+vehiculo = Vehiculo("BMW", "X4", "Negro", 0)
 
 while True:
     print("Menú: \n 1. Ver datos del vehiculo\t \n 2. Acelerar auto \t \n 3. Frenar auto \t \n 4. Salir")
