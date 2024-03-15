ff7419e89117a0811b9c23918c11b0adeb81f089
njofredev <nico.jofreandrade@gmail.com>
2024-03-15 09:52:05 -0300

    Add vehiculo.py base solution

--- proyecto-final-modulo-4/vehiculo.py
+++ proyecto-final-modulo-4/vehiculo.py
@@ -10,3 +10,27 @@ Nicolás Jofré Andrade
 
 - Imprimir estado del vehículo después de cada método
 """
+# Clase de vehículo
+class Vehiculo:
+
+    def __init__(self, marca, modelo, color, velocidad, velocidad_actual):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+        self.velocidad = velocidad
+        self.velocidad_actual = velocidad_actual
+    
+    def datos_actuales(self):
+        print(f"Los datos del auto son: {self.marca}/{self.modelo}/{self.color}/va a:{self.velocidad_actual}km/h")
+
+    def acelerar(self):
+        self.velocidad += 10
+        print(f"El auto marca: {self.marca}, modelo: {self.modelo}, color: {self.color} va a: {self.velocidad}km/h")
+        
+    def frenar(self):
+        self.velocidad -= 5
+        print(f"El auto marca: {self.marca}, modelo: {self.modelo}, color: {self.color} va a: {self.velocidad}km/h")
+
+vehiculo = Vehiculo("BMW", "X4", "Negro", 0, 0)
+
+vehiculo.datos_actuales()
